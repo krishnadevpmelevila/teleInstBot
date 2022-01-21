476f0147e0e94f19297893136a0771972b836bf2
Krishnadev P Melevila <krishnadev@localhost.localdomain>
2022-01-21 18:28:57 +0530

    final

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import re
 
 
 def download_image(url: str):
-
+ 
     # post request to server
 
     api = 'https://instasave.website/insta-downloader'
@@ -22,31 +22,104 @@ def download_image(url: str):
 
     soup = BeautifulSoup(x.text, 'html.parser')
 
-    img=soup.find('div', id='downloadBox').find('img')['src']
+    img=soup.find('div', id='downloadBox').findAll('a')
+
 
     return img
+def sdownload_story(url: str,num:int):
 
-# post request to server 
-def download_story(url: str,num:int):
-    print(num)
 
     api = 'https://instasave.website/instagram-stories-downloader'
-    print(url.replace(" ", ""))
-    myobj = {'username': url.replace(" ", "")}
+    url=url.replace(str(num), "")
+    link=url.replace(" ",'')
+    myobj = {'username': link}
 
     x = requests.post(api, data = myobj)
     # grab image from response
 
     soup = BeautifulSoup(x.text, 'html.parser')
     num=int(num)-1
-    content=soup.find('div', id='downloadBox').findAll('a')[num]['href']
-    return content
+    try:
+        content=soup.find('div', id='downloadBox').findAll('a')[num]['href']
+    except AttributeError as aerror:
+        return 0
+    else:
+        return content
+
+
+def sstory(update: Update, context: CallbackContext):
+    uuid = update.message.text.replace("/ss", "")
+    try:
+        if(uuid.__len__()!=0):
+            num=int(uuid[1])
+            if(uuid==''):
+                update.message.reply_text("Please use command /ss <URL>")
+                
+            else:
+                update.message.reply_text("Proccessing... ETA: 20 Seconds")
+                img = sdownload_story(uuid,num)
+                if img ==0:
+                    update.message.reply_text("Sorry, this story is not available! May be it is a private account, or it is not a story, or the user doesnot have "+num+' stories')
+                else:
+                    update.message.reply_video(img)
+        else:
+            update.message.reply_text("Enter the correct command syntax - /ss <story number of user> <username without @>")
+    except:
+        update.message.reply_text("Enter the correct command syntax - /ss <story number of user> <username without @>") 
+
+    
+    
+        
+# post request to server 
+def download_story(url: str):
+    api = 'https://instasave.website/instagram-stories-downloader'
+    if(url.startswith('https://www')):
+        link=url.replace("https://www.instagram.com/stories/","")
+        res = link[:link.index('/') + len('/')]
+        res=res.replace('/', '')
+        res=res.replace(' ', '')
+        print(res)
+        myobj = {'username': res}
+        x = requests.post(api, data = myobj)
+        # grab image from response
+
+        soup = BeautifulSoup(x.text, 'html.parser')
+        try:
+            content=soup.find('div', id='downloadBox').findAll('a')
+        except AttributeError as aerror:
+            return 0
+        else:
+            return content
+
+        
+    else:
+        link=url.replace("https://instagram.com/stories/","")
+        res = link[:link.index('/') + len('/')]
+        res=res.replace('/', '')
+        res=res.replace(' ', '')
+        myobj = {'username': res}
+        x = requests.post(api, data = myobj)
+        # grab image from response
+
+        soup = BeautifulSoup(x.text, 'html.parser')
+        try:
+            content=soup.find('div', id='downloadBox').findAll('a')
+        except AttributeError as aerror:
+            return 0
+        else:
+            return content
+        
+        
+        
+
+
+    
 
 
 
 
 
-updater = Updater("5027109401:AAEl7fu-lG9c-LZF2WdtxmlkZkls34DxzC0",use_context=True)
+updater = Updater("5027109401:AAHlC-Pl8wBLce8Qmbop3qNSBYrAtaQh0QM",use_context=True)
 
 
 def start(update: Update, context: CallbackContext):
@@ -57,38 +130,49 @@ def start(update: Update, context: CallbackContext):
 
 def help(update: Update, context: CallbackContext):
     update.message.reply_text("""Available Commands :-
-    /download <URL> - To download instagram image/video/story (Url must start with http:// or https://)
+    /post <URL> - To download instagram posts (Url must start with http:// or https://)
+    /ss <story number> <username without @> - To download single story of a user)
+    /story <any story link of user> - To download all stories of a user
     /help - To see the this menu""")
 def download(update: Update, context: CallbackContext):
-    uuid = update.message.text.replace("/download", "")
+    uuid = update.message.text.replace("/post", "")
     if(uuid==''):
-        update.message.reply_text("Please use command /download <URL>")
-        update.message.reply_text(uuid)
+        update.message.reply_text("Please use command /post <URL>")
+
     else:
-        update.message.reply_text(uuid.startswith(' http'))
+        if(uuid.startswith(' http')):
+           
+
+            update.message.reply_text("Proccessing... Please wait for 20 seconds")
+            img = download_image(uuid)
+            for i in img:
+                try:
+                    update.message.reply_video(i['href'])
+                except:
+                    update.message.reply_text("Ente Ponnadave! nee enth link aa thanne? Server adichu poyallo!!")
+                
+        else:
+            update.message.reply_text("Please use command /post <URL> with a valid url starting with http:// Or https://")
+
+def downloadstory(update: Update, context: CallbackContext):
+    uuid = update.message.text.replace("/story", "")
+    if(uuid==''):
+        update.message.reply_text("Please use command /story <URL>") 
+    else:
         
         if(uuid.startswith(' http')):
            
 
             update.message.reply_text("Downloading... ETA: 1 Minute")
-            img = download_image(uuid)
-            update.message.reply_photo(photo=img)
+            img = download_story(uuid)
+            if img ==0:
+                update.message.reply_text("Sorry, this story is not available! May be it is a private account, or it is not a story, or the url entered by you is not in the form of https://instagram.com/stories/<username>/<story_id>")
+            else:
+                for each in img:
+                    link=each['href']
+                    update.message.reply_video(link)
         else:
-            update.message.reply_text(uuid)
-            update.message.reply_text("Please use command /download <URL> with a valid url starting with http:// Or https://")
-
-def downloadstory(update: Update, context: CallbackContext):
-    uuid = update.message.text.replace("/downloadstory", "")
-    num=int(uuid[1])
-    print(uuid[1])
-    if(uuid==''):
-        update.message.reply_text("Please use command /download <URL>")
-        update.message.reply_text(uuid)
-    else:
-        update.message.reply_text(uuid.startswith(' http'))
-        update.message.reply_text("Downloading... ETA: 1 Minute")
-        img = download_story(uuid,num)
-        update.message.reply_video(img)
+            update.message.reply_text("Please use command /story <URL> with a valid url starting with http:// Or https://")
         
 
 
@@ -103,9 +187,10 @@ def unknown_text(update: Update, context: CallbackContext):
 
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('download', download))
-updater.dispatcher.add_handler(CommandHandler('downloadstory', downloadstory))
+updater.dispatcher.add_handler(CommandHandler('post', download))
+updater.dispatcher.add_handler(CommandHandler('story', downloadstory))
 updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('ss', sstory))
 
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
 updater.dispatcher.add_handler(MessageHandler(
